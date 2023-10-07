862396102afeb8d3f49b2e3ffe7c3706b69f1044
silvianaim02 <silvianaim68@gmail.com>
2023-10-07 17:47:57 +0700

    editNoteByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -82,4 +82,55 @@ const getNoteByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, tags, body } = request.payload;
+
+  // Validasi input
+  if (
+    typeof title !== 'string' ||
+    !Array.isArray(tags) ||
+    typeof body !== 'string'
+  ) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Invalid input. title harus string, tags harus array, dan body harus string.',
+    });
+    response.code(400); // Bad Request
+    return response;
+  }
+  const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+};
